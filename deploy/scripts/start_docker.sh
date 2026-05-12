efe0e3b819bfcbd78f9dd835af0d3f6fb8e144b8
yogibaba7 <yogeshchouhan263@gmail.com>
2026-05-12 16:17:06 +0530

    something

--- deploy/scripts/start_docker.sh
+++
@@ -1,6 +0,0 @@
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 128529977235.dkr.ecr.eu-north-1.amazonaws.com
-docker pull 128529977235.dkr.ecr.eu-north-1.amazonaws.com/pulsecore_dockerimg_repo:latest
-docker stop my-container || true
-docker rm my-container || true
-docker run -d -p 80:8000 --name pulsecore-app -e PULSECORE_PAT=edd9838e65f7b41e3b8c5c6d13e3b5ee7b4bab42 -e PULSECORE_USERNAME=yogibaba7 -e YOUTUBEAPI_KEY=AIzaSyD1OnVEoYqN1BOZK57zGeJR0VWOaGDuRLc 128529977235.dkr.ecr.eu-north-1.amazonaws.com/pulsecore_dockerimg_repo:latest
-
